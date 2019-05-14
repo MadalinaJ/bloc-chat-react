c7784e844ea4dff95638be4de3599d7aae2514f1
Madalina Jantea <madalina.jantea@gmail.com>
2019-05-14 00:25:08 -0700

    modif RoomList.js App.css

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -7,6 +7,7 @@ class RoomList extends Component {
         rooms: [],
       }
       this.roomsRef = this.props.firebase.database().ref('rooms');
+      this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -17,11 +18,78 @@ class RoomList extends Component {
       });
     }
 
+    handleChange(e){
+      this.setState({name: e.target.value});
+    }
+    createRoom(newRoomName) {
+      this.roomsRef.push({
+        name: newRoomName
+      });
+      this.setState({name: newRoomName});
+      this.setState({ showFormForNewRoom: false });
+    }
+
+
+    formRoomOpen(){
+      this.setState({ showFormForNewRoom:true });
+
+
+    }
+
+    formRoomClose(){
+      this.setState({showFormForNewRoom:false });
+    }
+
+
+    handleFormRoomChangeOnPage(){
+       if(!this.state.showFormForNewRoom){
+       this.formRoomOpen();
+    } else{
+       this.formRoomClose();
+    }
+    }
+
+    handleButtonForRoomChange(){
+     if(!this.state.showFormForNewRoom){
+       return (
+         <span>New Room</span>
+       );
+     } else{
+       return (
+         <span>Cancel</span>
+       );
+     }
+    }
+
+    showForm(){
+      if(this.state.showFormForNewRoom){
+        return (
+          <form id="newRoomForm">
+            <h3>Create a new room </h3>
+           <input type="text" id="newRoomName" name="newRoomName" placeholder="Enter room name..."
+               onChange={ this.handleChange }value={this.state.name}></input>
+           <div className="bt-create-room">
+             <input type="button" id="submit" name="submit" value="Create Room" onClick={ () => this.createRoom(this.state.name)}></input>
+           </div>
+
+          </form>
+        );
+      }
+      else{
+        return null;
+      }
+    }
+
+
+
     render() {
     return (
        <section className="RoomList">
        <div className="rooms">
               <h2 className="header">Bloc Chat</h2>
+
+              <button className="new-room" onClick={ () => this.handleFormRoomChangeOnPage()}> { this.handleButtonForRoomChange() }</button>
+              <div className="new-room-form-open">{this.showForm()}</div>
               <table>
               <tbody>
               {
